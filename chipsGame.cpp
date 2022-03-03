b87a9ce69b845191bb6d625666c4301a476dc2a6
Clinton Holladay <86504167+ClintonHolladay@users.noreply.github.com>
2022-03-03 11:32:59 -0600

    Update chipsGame.cpp
    Added registration and Clearance of the console screen

--- chipsGame.cpp
+++ chipsGame.cpp
@@ -2,9 +2,10 @@
 #include <ctime>
 #include <cstdlib>
 #include <fstream>
+#include<string>
 
-using namespace std; 
- 
+using namespace std;
+
 const float MAX_TURN = .5;
 const int MAX_CHIPS = 100;
 bool player1Turn = true;
@@ -13,16 +14,135 @@ bool continueGame = true;
 int chipsInPile = 0;
 int chipsTaken = 0;
 int player1Wins = 0;
-int computerWins =0;
+int computerWins = 0;
 
 string playerName;
 
+//Function Declarations//
+//================================================================================//
 void introduction();
 void gameLoop();
 void playAgain();
 
-int main()  
-{ 
+int main()
+{
+    bool accessGranted = false;
+    int choice;
+    int counter = 0;
+    string username;
+    string password;
+    string passwordTry;
+    string usernameTry;
+    cout << "Please login to access the game. If you have not registered you will need to register before you can login.\n" << endl;
+    cout << "Choose an option: " << endl;
+    cout << "1: Register.\n2: Login." << endl;
+    while (!accessGranted)
+    {
+        bool invalidAnswer = true;
+        while (invalidAnswer)
+        {
+            cin >> choice;
+            if (choice == 1)
+            {
+                invalidAnswer = false;
+                cout << "Your username & password need to be a single line of characters." << endl;
+                cout << "Enter your username: " << endl;
+                cin >> username;
+                cout << "Enter your password: " << endl;
+                cin >> password;
+                system("CLS");
+
+                ofstream registerFile;
+                registerFile.open("username.txt");
+                if (registerFile.fail())
+                {
+                    cout << "Register file username failed to open." << endl;
+                }
+                else
+                {
+                    registerFile << username << endl;
+                }
+                registerFile.close();
+
+                registerFile.open("password.txt");
+                if (registerFile.fail())
+                {
+                    cout << "Register file password failed to open." << endl;
+                }
+                else
+                {
+                    registerFile << password << endl;;
+                }
+                registerFile.close();
+
+                cout << "Thank you for registering! You are able to login now.\n" << endl;
+                cout << "Choose an option: " << endl;
+                cout << "1: Register.\n2: Login." << endl;
+
+                //accessGranted = true;
+            }
+            else if (choice == 2)
+            {
+                invalidAnswer = false;
+                cout << "Please enter your username: " << endl;
+                cin >> usernameTry;
+                cout << "Please enter your password: " << endl;
+                cin >> passwordTry;
+                string un;
+                string pw;
+                ifstream loginFile;
+                loginFile.open("username.txt");
+                if (loginFile.fail())
+                {
+                    cout << "Login username file failed to open." << endl;
+                }
+                else
+                {
+                    loginFile >> un;
+                }
+                loginFile.close();
+
+                loginFile.open("password.txt");
+                if (loginFile.fail())
+                {
+                    cout << "Login password file failed to open." << endl;
+                }
+                else
+                {
+                    loginFile >> pw;
+                }
+                loginFile.close();
+
+                if (un == usernameTry && pw == passwordTry)
+                {
+                    accessGranted = true;
+                    system("CLS");
+                }
+                else
+                {
+                    accessGranted = false;
+                    cout << "Your credentials don't match the saved file data. You have not been granted access. Try again.\n" << endl;
+                    cout << "Choose an option: " << endl;
+                    cout << "1: Register.\n2: Login." << endl;
+                    invalidAnswer = true;
+                    counter++;
+                    if (counter > 2)
+                    {
+                        cout << "\n\n*****To many login attempts have been made... Access DENIED.*****\n\n" << endl;
+                        return 0;
+                    }
+                }
+            }
+            else
+            {
+                cout << "You entered an invalid answer... Please try again." << endl;
+                invalidAnswer = true;
+            }
+        }
+
+    }
+
+
     while (continueGame)
     {
         ifstream inputFile;
@@ -44,13 +164,13 @@ int main()
         srand(time(0));
         introduction();
         gameLoop();
-        playAgain();     
+        playAgain();
     }
-    cout << "Goodbye!! Thanks for playing!!" << endl;  
-  return 0; 
-} 
+    cout << "Goodbye!! Thanks for playing!!" << endl;
+    return 0;
+}
 
-                            //Function Definitions//
+//Function Definitions//
 //================================================================================//
 
 void introduction()
@@ -70,7 +190,7 @@ void gameLoop()
     while (!gameOver)
     {
         int maxToTake = MAX_TURN * chipsInPile;
-        cout << maxToTake << " Chips can be taken from the pile." << endl;
+        cout <<"*****"<< maxToTake << "***** Chips can be taken from the pile." << endl;
         int playerTaken = 0;
         if (player1Turn)
         {
@@ -116,7 +236,7 @@ void gameLoop()
             {
                 cout << "The Computer WON!!!" << endl;
                 computerWins++;
-                cout <<"The Computer has won " << computerWins << " times :( :(" << endl;
+                cout << "The Computer has won " << computerWins << " times :( :(" << endl;
                 ofstream outputFile;
                 outputFile.open("ComputerCount.txt");
                 if (!outputFile.fail())
@@ -142,11 +262,13 @@ void playAgain()
         {
             continueGame = true;
             validAnswer = false;
+            system("CLS");
         }
         else if (game == 'N')
         {
             continueGame = false;
             validAnswer = false;
+            system("CLS");
         }
         else
             cout << "Invalid input! try again..." << endl;
