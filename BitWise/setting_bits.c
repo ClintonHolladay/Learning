c41c8b33d5663046b2eecc6ba330ea5888e0079f
Clinton Holladay <86504167+ClintonHolladay@users.noreply.github.com>
2022-11-23 08:00:08 -0600

    Create setting_bits.c

---
+++ BitWise/setting_bits.c
@@ -0,0 +1,36 @@
+/*
+ * main_2.c
+ *
+ *  Created on: Nov 22, 2022
+ *      Author: Clinton Holladay
+ */
+
+
+#include<stdio.h>
+#include<stdint.h>
+
+
+void wait_for_user();
+
+int main(void)
+{
+	for(;;)
+	{
+		int32_t a;
+		int32_t MASK = 0b00011;//set positions 0 and 1 with |
+		printf("Enter a number.\n");
+		scanf("%d",&a);
+		a |= MASK;
+		printf("Your number is now: %d\n", a);
+
+		wait_for_user();
+	}
+
+}
+
+void wait_for_user()//hang program to wait for user input
+{
+	printf("Press any key to EXIT\n");
+	while(getchar()!= '\n'){}
+	getchar();
+}
