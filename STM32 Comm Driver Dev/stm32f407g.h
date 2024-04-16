3381e2e02e1c43536831a2e537dd5564f7b2aa0a
Clinton Holladay <86504167+ClintonHolladay@users.noreply.github.com>
2024-04-15 23:49:38 -0500

    Add files via upload

--- STM32 Comm Driver Dev/stm32f407g.h
+++ STM32 Comm Driver Dev/stm32f407g.h
@@ -8,6 +8,8 @@
 #ifndef INC_STM32F407G_H_
 #define INC_STM32F407G_H_
 
+#include <stdint.h>
+
 // Base Address MACROs
 
 #define FLASH_BASEADDR				0x08000000U
@@ -34,6 +36,7 @@
 #define GPIOG_BASEADDR				0x40021800U
 #define GPIOH_BASEADDR				0x40021C00U
 #define GPIOI_BASEADDR				0x40022000U
+#define RCC_BASEADDR				0x40023800U
 
 // APB1 Peripheral Base Addresses
 
@@ -59,26 +62,83 @@
 
 typedef struct
 {
-	volatile uint32_t MODER;
-	volatile uint32_t OTYPER;
-	volatile uint32_t OSPEEDR;
-	volatile uint32_t PUPDR;
-	volatile uint32_t IDR;
-	volatile uint32_t ODR;
-	volatile uint32_t BSRR;
-	volatile uint32_t LCKR;
-	volatile uint32_t AFRL;
-	volatile uint32_t AFRH;
+	volatile uint32_t MODER;		//GPIO port mode register
+	volatile uint32_t OTYPER;		//GPIO port output type register
+	volatile uint32_t OSPEEDR;		//GPIO port output speed register
+	volatile uint32_t PUPDR;		//GPIO port pull-up/pull-down register
+	volatile uint32_t IDR;			//GPIO port input data register
+	volatile uint32_t ODR;			//GPIO port output data register
+	volatile uint32_t BSRR;			//GPIO port bit set/reset register
+	volatile uint32_t LCKR;			//GPIO port configuration lock register
+	volatile uint32_t AFRL;			//GPIO alternate function low register
+	volatile uint32_t AFRH;			//GPIO alternate function high register
 }; GPIO_RegDef_t;
 
-#define GPIOA				((GPIO_RegDef_t*)GPIOA_BASEADDR)
-#define GPIOB				((GPIO_RegDef_t*)GPIOB_BASEADDR)
-#define GPIOC				((GPIO_RegDef_t*)GPIOC_BASEADDR)
-#define GPIOD				((GPIO_RegDef_t*)GPIOD_BASEADDR)
-#define GPIOE				((GPIO_RegDef_t*)GPIOE_BASEADDR)
-#define GPIOF				((GPIO_RegDef_t*)GPIOF_BASEADDR)
-#define GPIOG				((GPIO_RegDef_t*)GPIOG_BASEADDR)
-#define GPIOH				((GPIO_RegDef_t*)GPIOH_BASEADDR)
-#define GPIOI				((GPIO_RegDef_t*)GPIOI_BASEADDR)
+// RCC Reset Clock Control Register
+
+typedef struct
+{
+	volatile uint32_t RCC_CR;		//RCC clock control register
+	volatile uint32_t PLLCFGR;		//RCC PLL configuration register
+	volatile uint32_t CFGR;			//RCC clock configuration register
+	volatile uint32_t CIR;			//RCC clock interrupt register
+	volatile uint32_t AHB1RSTR;		//RCC AHB1 peripheral reset register
+	volatile uint32_t AHB2RSTR;		//RCC AHB2 peripheral reset register
+	volatile uint32_t AHB3RSTR;		//RCC AHB3 peripheral reset register
+	volatile uint32_t Reserved1;
+	volatile uint32_t APB1RSTR;		//RCC APB1 peripheral reset register
+	volatile uint32_t APB2RSTR;		//RCC APB2 peripheral reset register
+	volatile uint32_t Reserved2;
+	volatile uint32_t Reserved3;
+	volatile uint32_t AHB1ENR;		//RCC AHB1 peripheral clock register
+	volatile uint32_t AHB2ENR;		//RCC AHB2 peripheral clock enable register
+	volatile uint32_t AHB3ENR;		//RCC AHB3 peripheral clock enable register
+	volatile uint32_t Reserved4;
+	volatile uint32_t APB1ENR;		//RCC APB1 peripheral clock enable register
+	volatile uint32_t APB2ENR;		//RCC APB2 peripheral clock enable register
+	volatile uint32_t Reserved5;
+	volatile uint32_t Reserved6;
+	volatile uint32_t AHB1LPENR;	//RCC AHB1 peripheral clock enable in low power mode register
+	volatile uint32_t AHB2LPENR;	//RCC AHB2 peripheral clock enable in low power mode register
+	volatile uint32_t AHB3LPENR;	//RCC AHB3 peripheral clock enable in low power mode register
+	volatile uint32_t Reserved7;
+	volatile uint32_t APB1LPENR;	//RCC APB1 peripheral clock enable in low power mode register
+	volatile uint32_t APB2LPENR;	//RCC APB2 peripheral clock enabled in low power mode register
+	volatile uint32_t Reserved8;
+	volatile uint32_t Reserved9;
+	volatile uint32_t BDCR;			//RCC Backup domain control register
+	volatile uint32_t CSR;			//RCC clock control & status register
+	volatile uint32_t Reserved10;
+	volatile uint32_t Reserved11;
+	volatile uint32_t SSCGR;		//ll RCC spread spectrum clock generation register
+	volatile uint32_t PLLI2SCFGR; 	//RCC PLLI2S configuration register
+	volatile uint32_t PLLSAICFGR;	//RCC PLL configuration register
+	volatile uint32_t DCKCFGR;		//RCC Dedicated Clock Configuration Register
+
+}; RCC_RegDef_t;
+
+#define GPIOA						((GPIO_RegDef_t*)GPIOA_BASEADDR)
+#define GPIOB						((GPIO_RegDef_t*)GPIOB_BASEADDR)
+#define GPIOC						((GPIO_RegDef_t*)GPIOC_BASEADDR)
+#define GPIOD						((GPIO_RegDef_t*)GPIOD_BASEADDR)
+#define GPIOE						((GPIO_RegDef_t*)GPIOE_BASEADDR)
+#define GPIOF						((GPIO_RegDef_t*)GPIOF_BASEADDR)
+#define GPIOG						((GPIO_RegDef_t*)GPIOG_BASEADDR)
+#define GPIOH						((GPIO_RegDef_t*)GPIOH_BASEADDR)
+#define GPIOI						((GPIO_RegDef_t*)GPIOI_BASEADDR)
+
+#define RCC							((RCC_RegDef_t*)RCC_BASEADDR)
+
+
+#define GPIOA_PCLK_EN				(RCC->AHB1ENR |= (1 << 0))
+#define GPIOB_PCLK_EN
+#define GPIOC_PCLK_EN
+#define GPIOD_PCLK_EN
+#define GPIOE_PCLK_EN
+#define GPIOF_PCLK_EN
+#define GPIOG_PCLK_EN
+#define GPIOH_PCLK_EN
+#define GPIOI_PCLK_EN
+
 
 #endif /* INC_STM32F407G_H_ */
